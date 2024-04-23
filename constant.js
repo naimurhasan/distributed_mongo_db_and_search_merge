460119c9ec79b4f68add2d5c26b27c87a67ed08c
Naimur Hasan <37500414+naimurhasan@users.noreply.github.com>
2024-04-23 17:49:38 +0600

    Update constant.js

--- constant.js
+++ constant.js
@@ -1,6 +1,6 @@
 module.exports = {
-    EDUCATION_URI: 'mongodb+srv://nivevis937:gowSjUJoyBEhNs7W@cluster0.kkbyhnh.mongodb.net/education?retryWrites=true&w=majority&appName=Cluster0',
-    WORK_URI: 'mongodb+srv://conamej954:lqR6pi8ostgX7UDi@cluster0.oyplcsi.mongodb.net/work?retryWrites=true&w=majority&appName=Cluster0',
-    RESEARCH_URI: 'mongodb+srv://yigex45140:W0QuKRaUth8DvLfs@cluster0.osdvfr5.mongodb.net/research?retryWrites=true&w=majority&appName=Cluster0',
+    EDUCATION_URI: 'mongodb+srv://nivevis937:@cluster0.kkbyhnh.mongodb.net/education?retryWrites=true&w=majority&appName=Cluster0',
+    WORK_URI: 'mongodb+srv://conamej954:@cluster0.oyplcsi.mongodb.net/work?retryWrites=true&w=majority&appName=Cluster0',
+    RESEARCH_URI: 'mongodb+srv://yigex45140:@cluster0.osdvfr5.mongodb.net/research?retryWrites=true&w=majority&appName=Cluster0',
 };
   
